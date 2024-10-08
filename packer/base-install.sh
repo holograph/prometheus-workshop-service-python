897de2323f79082cae4cbdab6ad022d35cbc6dd8
Tomer Gabel <tomer@tomergabel.com>
2024-10-08 19:16:21 +0300

    Set up helper public key + MOTD

--- packer/base-install.sh
+++ packer/base-install.sh
@@ -17,6 +17,34 @@ sudo apt-get install -y                           \
 echo '- Setting up Linux desktop and lab user'
 sudo apt-get install -y ubuntu-desktop-minimal
 sudo useradd -g sudo -m -s /bin/bash -p $(echo "student" | openssl passwd -1 -stdin) student
+sudo mkdir -p /home/student/.ssh
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEz4jal99UkJ8EOL/oTQQRvlRZa+gF8PXI1PeEl/+y35 lab@example.com' \
+  | sudo tee /home/student/.ssh/authorized_keys
+sudo chown -R student /home/student/.ssh
+
+public_ip="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+cat <<EOF | sudo tee /etc/motd
+Welcome to your Prometheus workshop lab instance!
+
+The two most important links you will need are:
+* http://${public_ip}:8080/docs - The example service for this workshop
+* http://${public_ip}:3000 - Grafana
+
+Your instance's public IP is ${public_ip}, and provides the following SSH access via:
+  ssh -i <private_key> student@${public_ip}
+The private key is available from the classroom materials.
+
+Running scenarios with the example service:
+* curl "${service_url}/scenario/health"
+  Shows the status of the currently running scenario (if any), as well as the names of available scenarios
+* curl -XPOST "${service_url}/scenario/<name>?action=<action>"
+  Performs an action on a particular scenario. Available actions include "start" and "stop" (no quotes)
+
+Additionally, if desired, you can also access Prometheus directly via: http://${public_ip}:9090
+
+Good luck!
+
+EOF
 
 echo '--- Installing lab components ---'
 ./service-install.sh
